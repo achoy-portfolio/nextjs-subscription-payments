c6ed20b5a9fd021a9fd2581bba7f02833bacb61f
Alan <alan.choyz@gmail.com>
2024-11-19 22:20:20 -0700

    change font color

--- components/ui/Pricing/Pricing.tsx
+++ components/ui/Pricing/Pricing.tsx
@@ -107,12 +107,11 @@ export default function Pricing({ user, products, subscription }: Props) {
       <section className="bg-white">
         <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
           <div className="sm:flex sm:flex-col sm:align-center">
-            <h1 className="text-4xl font-extrabold text-black sm:text-center sm:text-6xl">
+            <h1 className="text-4xl font-extrabold text-green-700 sm:text-center sm:text-6xl">
               Pricing Plans
             </h1>
-            <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-              Start building for free, then add a site plan to go live. Account
-              plans unlock additional features.
+            <p className="max-w-2xl m-auto mt-5 text-xl text-grey-600 sm:text-center sm:text-2xl">
+              Cancel Anytime
             </p>
             <div className="relative self-center mt-6 bg-white-900 rounded-lg p-0.5 flex sm:mt-8 border border-zinc-800">
               {intervals.includes('month') && (
@@ -123,7 +122,7 @@ export default function Pricing({ user, products, subscription }: Props) {
                     billingInterval === 'month'
                       ? 'relative w-1/2 bg-white border-zinc-800 shadow-sm text-black'
                       : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                  } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+                  } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
                 >
                   Monthly billing
                 </button>
@@ -136,7 +135,7 @@ export default function Pricing({ user, products, subscription }: Props) {
                     billingInterval === 'year'
                       ? 'relative w-1/2 bg-white border-zinc-800 shadow-sm text-black'
                       : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                  } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+                  } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
                 >
                   Yearly billing
                 </button>
@@ -160,7 +159,7 @@ export default function Pricing({ user, products, subscription }: Props) {
                   className={cn(
                     'flex flex-col rounded-lg shadow-sm divide-y divide-zinc-600 bg-white-900',
                     {
-                      'border border-pink-500': subscription
+                      'border border-green-500': subscription
                         ? product.name === subscription?.prices?.products?.name
                         : product.name === 'Freelancer'
                     },
@@ -173,12 +172,12 @@ export default function Pricing({ user, products, subscription }: Props) {
                     <h2 className="text-2xl font-semibold leading-6 text-black">
                       {product.name}
                     </h2>
-                    <p className="mt-4 text-zinc-300">{product.description}</p>
+                    <p className="mt-4 text-zinc-900">{product.description}</p>
                     <p className="mt-8">
                       <span className="text-5xl font-extrabold white">
                         {priceString}
                       </span>
-                      <span className="text-base font-medium text-zinc-100">
+                      <span className="text-base font-medium text-green-700">
                         /{billingInterval}
                       </span>
                     </p>
